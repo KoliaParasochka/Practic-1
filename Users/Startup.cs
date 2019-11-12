9638304ebed25827d212c1041987626e31ef878b
Kolia Parasochka <koliaparasochka99@gmail.com>
2019-11-12 15:47:01 +0200

    Finished!

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -6,13 +6,13 @@ using Domain.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectDb.EF;
 using ProjectDb.Initial;
 using ProjectDb.Storage;
+using Microsoft.Extensions.Hosting;
 
 namespace Users
 {
@@ -36,10 +36,7 @@ namespace Users
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddNodeServices(options =>
-            //{
-            //    options.ProjectPath = "Path/That/Doesnt/Exist";
-            //});
+            
             services.AddDbContext<ApplicationContext>(option
                 => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -50,7 +47,7 @@ namespace Users
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             using (var scope = app.ApplicationServices.CreateScope())
             {
@@ -62,11 +59,7 @@ namespace Users
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                // добавляем сборку через webpack
-                //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                //{
-                //    HotModuleReplacement = true
-                //});
+               
             }
             else
             {
@@ -75,16 +68,18 @@ namespace Users
             }
 
             app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+           
             app.UseStatusCodePages();
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseMvc(routes  =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=home}/{action=index}/{id?}");
-            });
+            
+            
             app.UseCors(builder => builder.WithOrigins("http://localhost:4200"));
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
         }
     }
 }
